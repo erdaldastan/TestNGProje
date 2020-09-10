aad8028400b57bc98cafef612edbf3cf6e84ee45
erdaldastan <erdaldastan69@gmail.com>
2020-09-11 00:08:15 +0300

    day18 ders sonu

---
+++ src/test/java/com/techproed/tests/FhcTripHotelCreateTest.java
@@ -0,0 +1,31 @@
+package com.techproed.tests;
+
+import com.techproed.pages.FhcTripHotelCreatePage;
+import com.techproed.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class FhcTripHotelCreateTest extends TestBase {
+
+    public void giris(){
+        driver.get("http://fhctrip-qa.com/admin/HotelAdmin/Create");
+        driver.findElement(By.id("UserName")).sendKeys("manager2");
+        driver.findElement(By.id("Password")).sendKeys("Man1ager2!"+ Keys.ENTER);
+    }
+    @Test
+    public void test(){
+        giris();
+        FhcTripHotelCreatePage fhcTripHotelCreatePage = new FhcTripHotelCreatePage(driver);
+        fhcTripHotelCreatePage.codeBox.sendKeys("1234");
+        fhcTripHotelCreatePage.addressBox.sendKeys("Çıkmaz sk.");
+        fhcTripHotelCreatePage.emailBox.sendKeys("techproed@techproed.com");
+        fhcTripHotelCreatePage.nameBox.sendKeys("Techproed");
+        fhcTripHotelCreatePage.phoneBox.sendKeys("1223454534");
+
+        Select select = new Select(fhcTripHotelCreatePage.selectBox);
+        select.selectByIndex(2);
+        fhcTripHotelCreatePage.saveButton.click();
+    }
+}
